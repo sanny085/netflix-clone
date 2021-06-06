a8425b78d34f393a18dc04533f8d92efa07923be
Sanny Kumar <sannykumar085@gmail.com>
2021-06-06 17:27:38 +0530

    Navbar Created

---
+++ src/Nav.js
@@ -0,0 +1,34 @@
+import React,{ useState, useEffect } from 'react'
+import './Nav.css'
+
+
+const Nav = () => {
+   const [navAnima, setNavAnima] =useState(false);
+
+   const AnimationHandler = () =>{ 
+      if(window.scrollY >100){
+        setNavAnima(true);
+      }
+      else{
+        setNavAnima(false);
+      }
+    };
+
+   useEffect(()=>{
+       window.addEventListener('scroll', AnimationHandler);
+       return () => {
+           window.removeEventListener('scroll', AnimationHandler);
+        }
+     },[]);
+
+    return (
+      <div className={`nav ${ navAnima && 'nav__black'}`}>
+         <div className="nav__contents">
+            <img className="float-start  nav__logo" src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="netflix brand logo"/>
+            <img className=" float-end rounded nav__avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="netflix avatar"/>
+         </div>
+      </div>
+    )
+}
+export default Nav;
+
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDEFm05584E2EQFN3aLQlBza3QvXmd2vac",
+    authDomain: "netflix-clone-dca4d.firebaseapp.com",
+    projectId: "netflix-clone-dca4d",
+    storageBucket: "netflix-clone-dca4d.appspot.com",
+    messagingSenderId: "697829610453",
+    appId: "1:697829610453:web:8b3b37bade6d6bb3cba35c",
+    measurementId: "G-MZC3ST0VBP"
+  };
