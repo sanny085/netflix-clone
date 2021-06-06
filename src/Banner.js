c5a03363e9ef79fd7253ad58a1129964cf444709
Sanny Kumar <sannykumar085@gmail.com>
2021-06-06 22:51:44 +0530

    update

--- src/Banner.js
+++ src/Banner.js
@@ -2,6 +2,10 @@ import React from 'react'
 import './Banner.css';
 
 const Banner = () => {
+
+    function truncate(string, n){
+        return string?.length > n ? string.substr(0, n-1) + "...." : string;
+    }
     return (
         <header className="banner" style={{  
             backgroundImage: `url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASIAAACuCAMAAAClZfCTAAAAA1BMVEUAAACnej3aAAAASElEQVR4nO3BMQEAAADCoPVPbQhfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABeA8XKAAFZcBBuAAAAAElFTkSuQmCC")`}}>
@@ -12,11 +16,16 @@ const Banner = () => {
                    <button className="banner__button">Play</button>
                    <button className="banner__button">My List</button>
                </div>
-               <h1 className="banner__description">This is test description</h1>
+               <h1 className="banner__description">
+                 {
+                  truncate(`This is test description. This is test description.
+                 This is test descriptionThis is test descriptionThis is test descriptionThis is test 
+                 descriptionThis is test descriptionThis is test descriptionThis is test description`,150)
+                 }
+               </h1>
             </div>
 
             <div className="banner--fadeBottom"/>
-
         </header>
     )
 }
---
+++ src/axios.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: "https://api.themoviedb.org/3"
+});
+
+export default instance;
+
+
+{/*7d161198e8f9b89da5beb6851bbeda63*/}
