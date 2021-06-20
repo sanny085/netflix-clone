39324e979fc52fde1e17d6f6650df23a346762c9
Sanny Kumar <sannykumar085@gmail.com>
2021-06-20 17:07:26 +0530

    Profile done

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,21 +1,31 @@
-import React, { useState, useEffect, useRef} from 'react';
+import React, { useState, useEffect} from 'react';
 import './SignUpScreen.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+import {useHistory} from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../features/userSlice';
 
 import {auth} from '../firebase';
 import db from '../firebase';
 
 const SignUpScreen = () => {
+ const user = useSelector(selectUser);
+ 
+ const  history = useHistory();
+
  const[email, setEmail] = useState('');
  const[password, setPassword] = useState('');
-  
+ 
+ 
  const register = (e) => {
       e.preventDefault();
       auth.createUserWithEmailAndPassword(email, password)
       .then( (userAuth) => {
           toast("Register Successfully!",{type:'success'},userAuth);         
           console.log("Register User: ",window.screen.width,userAuth); 
+          
       }) 
       .catch((error) => {
           var errorCode = error.code;
@@ -32,12 +42,19 @@ const SignUpScreen = () => {
      .then( (loginUser) =>{
          console.log("Login User Data",loginUser);
          toast(loginUser.user.email +" You have successfully login!",{type:'success'});  
+         
      })
      .catch( (error) => {
          toast(error.message,{type:'warning'});
          console.log("Login Error",error);
      });
 }
+
+useEffect(()=>{
+    if(user?.email) {
+        return history.push("/");
+    }
+});
  
  return(
   <div className="SignUpScreen mt-0 mb-3 ">
